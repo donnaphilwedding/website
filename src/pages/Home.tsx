33426189756aa6992ffa1f49e91edf0253c6b77b
Philip Tootill <philip-tootill@softwareimaging.com>
2022-09-14 19:37:39 +0100

    Tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 export const Home: FC = () => {
-    return <div className="text-3xl">
+    return <div className="text-3xl text-red-500">
         Home
     </div>
 }
