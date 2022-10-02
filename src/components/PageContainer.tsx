867d89d840d8751b14ccea6b9094b3ddde67230a
Philip Tootill <donnaphilwedding@gmail.com>
2022-10-02 21:39:09 +0100

    Added background image

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,21 @@
+import { FC, HTMLAttributes } from 'react';
+import BackgroundImage from './woodlands.jpeg';
+
+interface PageContainerProps extends HTMLAttributes<HTMLDivElement> {}
+
+export const PageContainer: FC<PageContainerProps> = ({ children }) => {
+  return (
+    <div className="relative">
+      <div
+        className="absolute w-full h-[90vh]"
+        style={{
+          backgroundImage: `url(${BackgroundImage})`,
+          backgroundPosition: '50% -500px',
+          backgroundRepeat: 'none'
+        }}
+      />
+      <div className="absolute w-full h-[90vh] bg-gradient-to-b from-primary/25 to-white" />
+      <div className="max-w-4xl mx-auto">{children}</div>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
 import { Header } from '../components/Header';
+import {PageContainer} from '../components/PageContainer';
 
 export const Home: FC = () => {
   return (
     <div>
       <Header />
+      <PageContainer>
+        <div className="w-full bg-primary">
+            Home
+        </div>
+      </PageContainer>
     </div>
   );
 };
