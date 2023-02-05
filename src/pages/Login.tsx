edf3424c1a094b0234c4a5302fce082b51ab0e18
Phil Tootill <donnaphilwedding@gmail.com>
2023-02-05 12:18:25 +0000

    Styling improvements

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,13 +29,25 @@ export const Login: FC = () => {
   return (
     <PageContainer noHeader>
       <div className="w-full h-screen flex items-center justify-center">
-        <div className="bg-white w-[350px] p-5">
+        <div className="bg-white w-[350px] rounded-lg overflow-hidden">
+          <div className="w-full p-5 bg-primary text-white">
+          <p className="font-calligraffitti text-4xl mb-[-6px] w-full text-center">{'D&P'}</p>
+            </div>
+          
           <form onSubmit={onSubmit}>
-            <div className="flex flex-col gap-2">
-              <input className="border border-gray-300 p-2 rounded-md" value={input} onChange={e => setInput(e.target.value)} placeholder={'Enter password'}></input>
+            <div className="flex flex-col gap-5 p-5">
+              <input
+                className="border border-gray-300 p-2 rounded-md"
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                placeholder={'Please enter password'}
+              ></input>
               {error && <div className="text-red-500">Incorrect password. Please try again.</div>}
-              <button className="bg-primary p-2 rounded-md text-gray-100 flex justify-center items-center h-8" type="submit">
-                {pretendingToLoad ? <Loader className='h-5 w-5'></Loader> : "Submit" }
+              <button
+                className="bg-primary p-2 rounded-md text-gray-100 flex justify-center items-center h-8"
+                type="submit"
+              >
+                {pretendingToLoad ? <Loader className="h-5 w-5"></Loader> : 'Enter'}
               </button>
             </div>
           </form>
