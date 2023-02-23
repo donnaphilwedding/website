b993a4337b7f1484a4e58d7ea59d3719e4f9ad05
Phil Tootill <donnaphilwedding@gmail.com>
2023-02-23 10:52:25 +0000

    Fix radio button colour

--- src/components/form/RadioButton.tsx
+++ src/components/form/RadioButton.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const RadioButton: FC<Props> = ({ label, checked, onClick }) => {
   return (
     <div className="flex gap-2 items-center">
-      <input type="radio" checked={checked} onChange={onClick} />
+      <input type="radio" checked={checked} onChange={onClick} className="accent-secondary" />
       <div>{label}</div>
     </div>
   );
