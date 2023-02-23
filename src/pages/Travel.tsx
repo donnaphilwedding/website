b0d577606a99e15052035d7719cff8177392e498
Phil Tootill <donnaphilwedding@gmail.com>
2023-02-23 10:02:22 +0000

    Add park and ride sites

--- src/pages/Travel.tsx
+++ src/pages/Travel.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { FC } from 'react';
 import { Card } from '../components/card/Card';
 import { PageContainer } from '../components/PageContainer';
@@ -15,8 +16,8 @@ export const Travel: FC = () => {
               <a className="underline" href="https://www.theairlineoxford.co.uk/">
                 Oxford Airline
               </a>{' '}
-              is a direct coach service. Buses leave from the central bus station every 30 minutes, and stop by
-              the Eastgate Hotel.
+              is a direct coach service. Buses leave from the central bus station every 30 minutes, and stop by the
+              Eastgate Hotel.
             </p>
           </Card>
           <Card secondary title="Arriving from London" collapsible>
@@ -31,25 +32,38 @@ export const Travel: FC = () => {
           </Card>
           <Card title="Arriving by train" collapsible>
             <p>
-              The train station is about a mile from the Eastgate Hotel. From the south, trains run directly from London Paddington or
-              Marylebone. To get here from the north, you'll probably need to change at Birmingham New Street.
+              The train station is about a mile from the Eastgate Hotel. From the south, trains run directly from London
+              Paddington or Marylebone. To get here from the north, you'll probably need to change at Birmingham New
+              Street.
             </p>
             <p>
-              Taxis are available outside the station, or it's a 15 minute walk to the Eastgate Hotel. Due to Oxford's traffic system, it will likely be faster to walk.
+              Taxis are available outside the station, or it's a 15 minute walk to the Eastgate Hotel. Due to Oxford's
+              traffic system, it will likely be faster to walk.
             </p>
           </Card>
           <Card secondary title="Arriving by car" collapsible>
             <p>
-              Driving in central Oxford is fairly difficult. The Eastgate Hotel has a very limited amount of parking, which needs to be booked in advance.
+              Driving in central Oxford is fairly difficult. The Eastgate Hotel has a very limited amount of parking,
+              which needs to be booked in advance.
             </p>
             <p>
-              If you’re planning to drive, the best plan is to leave your car at Thornhill Park and Ride, and take the
-              400 bus into the city.
+              If you’re planning to drive, the best plan is to leave your car at one of the park and ride sites around
+              the city, and get a bus into town.
             </p>
             <p>
               You could also park near our house in Forest Hill and get a taxi into the city. Let us know if you want to
               do this.
             </p>
+            <motion.iframe
+              layout
+              title={'Park and Rides Around Oxford'}
+              src={'https://www.google.com/maps/d/embed?mid=1tM_XZ6OL9FFR9vJeYKjLUBpHubM&ehbc=2E312F'}
+              height="300"
+              className="border rounded-sm"
+              allowFullScreen={false}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></motion.iframe>
           </Card>
         </div>
       </div>
